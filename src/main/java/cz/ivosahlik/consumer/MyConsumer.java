c26415bcd3835335726fae7cdfa392a5b7c9d52b
ivosahlik <ivosahlik@seznam.cz>
2023-07-21 14:49:45 +0200

    added header trace id and deserialization with mapstruct

--- src/main/java/cz/ivosahlik/consumer/MyConsumer.java
+++ src/main/java/cz/ivosahlik/consumer/MyConsumer.java
@@ -1,7 +1,10 @@
 package cz.ivosahlik.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.ivosahlik.dto.Customer;
+import cz.ivosahlik.event.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,6 +12,8 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class MyConsumer {
@@ -28,14 +33,23 @@ public class MyConsumer {
     )
     public void listen(
             @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
-//            @Header("trace_id") String traceId,
+            @Header("trace_id") String traceId,
 //            @Header(KafkaHeaders.CONSUMER) Consumer<String, String> consumer,
 //            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) String partitionId,
             @Payload String payload
     ) throws JsonProcessingException {
 
-        System.out.println("Key " + key);
-        System.out.println("Received new event " + payload);
+        try {
+            System.out.println("Key " + key + ", traceId " + traceId);
+            System.out.println("Received new event " + payload);
+
+            Event<Customer> event = mapper.readValue(payload, new TypeReference<>() {});
+            System.out.println(LocalDateTime.now() + " DESERIALIZATION - Received new event: " + event.eventId());
+
+        } catch (JsonProcessingException e) {
+            System.out.println("Error occurred");
+            throw e;
+        }
 
 //        System.out.println(consumer.groupMetadata().toString());
 //        System.out.println("Key " + key + " traceId " + traceId);
